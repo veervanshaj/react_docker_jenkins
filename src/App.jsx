b1455353f3894f6f26aeca5035e0a68047beb457
Veer <veervanshajwadehra@gmail.com>
2026-05-06 09:43:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>react app in ec 2 with jenkins</h1>
+      <h2>deploy by veer vanshaj</h2>
+    </div>
+  )
+}
+
+export default App
